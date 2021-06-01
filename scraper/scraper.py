94785b60fada60ddd4874fe1dd8bde3ec3897bd9
abdalazez <abdalazzezzalsalahat@icloud.com>
2021-06-01 09:46:16 +0200

    finaly solved

---
+++ scraper/scraper.py
@@ -0,0 +1,74 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+URL = "https://en.wikipedia.org/wiki/History_of_Mexico"
+
+
+def get_citations_needed_count(URL): 
+    ''' 
+    A function that takes in a url and returns an integer
+    Counts how many 'citation needed' in the article. 
+    '''
+
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(class_="mw-parser-output")
+
+    citation_needed=[]
+    for par in results:
+        try:
+            all_para = par.find_all('span', string = lambda text: 'citation' in text.lower())
+            if all_para :
+                for i in range(len(all_para)): 
+                    citation_needed.append(all_para[i])
+        except Exception as e:
+            continue
+    return len(citation_needed)
+
+def get_citations_needed_report(URL):
+
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    results = soup.find(class_="mw-parser-output")
+
+    all_paragraphs  = []
+    all_lines = []
+
+    resultsss = results.find_all('p')
+    for i in resultsss:
+        try:
+            all_para = i.find_all('span', string = lambda text: 'citation' in text.lower())
+            if all_para:
+                for j in range(len(all_para)):
+
+                    all_paragraphs.append(i.text) 
+
+                    pos = i.text.index('citation') 
+                    line = i.text[:pos-1].split(". ")
+                    all_lines.append(line[-1])
+
+                        
+        except Exception as e:
+            continue
+
+    output = ''
+    for p in range(len(all_paragraphs)):
+        output += f'citation {p+1}: \n'
+        output += f'{all_paragraphs[p]} \n'
+        output += f'So , we need citation after the below line: \n'
+        output += f'{all_lines[p]} \n\n\n'
+
+    f = open("citation.txt", "w")
+    f.write(output)
+    f.close()
+
+    return output 
+
+
+if __name__ == "__main__":
+
+    print(get_citations_needed_report(URL))
+    print(get_citations_needed_count(URL))
+
+
---
+++ tests/test_scraper.py
@@ -0,0 +1,14 @@
+from scraper.scraper import *
+
+
+def test_count():
+
+    URL = "https://en.wikipedia.org/wiki/History_of_Mexico"
+    assert get_citations_needed_count(URL) == 5
+
+def test_report():
+
+    URL = "https://en.wikipedia.org/wiki/History_of_Mexico"
+    expected = open("citation.txt", "r")
+    assert get_citations_needed_report(URL) == expected.read()
+
